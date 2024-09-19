09068e0a45097f7afd15fc61e96381879ec4d6d8
Chaman Kumar <chamansuper1@gmail.com>
2024-09-19 19:37:53 -0400

    Kanbas UI Ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import Labs from "./Labs";
+import Kanbas from "./kanbas";
+import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
+import TOC from "./TOC";
 
 export default function Labs() {
     return (
         <div>
+            <h1>Chaman Kumar</h1>
+            <TOC />
             <h1>Labs</h1>
-            <Lab1 />
-            <Lab2 />
-            <h1>Lab 3</h1>
+            <Routes>
+                <Route path="Lab1" element={<Lab1 />} />
+                <Route path="Lab2" element={<Lab2 />} />
+                <Route path="Lab3" element={<h2>Lab 3</h2>} />
+            </Routes>
         </div>
     );
 }
