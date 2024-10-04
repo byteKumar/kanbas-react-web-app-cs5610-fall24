2c536a1a4d228b7b5040f7050167cd18ca0d0b4a
Chaman Kumar <chamansuper1@gmail.com>
2024-10-03 22:35:54 -0400

    Adding id on Labs section

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,18 +1,41 @@
-import { Link } from "react-router-dom"
 import { useLocation } from "react-router";
-
-function TOC() {
+export default function TOC() {
   const { pathname } = useLocation();
   return (
     <ul className="nav nav-pills">
-      <li className="nav-item"><Link to="/Labs" className="nav-link">Labs</Link></li>
-      <li className="nav-item"><Link to="/Labs/Lab1" className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</Link></li>
-      <li className="nav-item"><Link to="/Labs/Lab2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</Link></li>
-      <li className="nav-item"><Link to="/Labs/Lab3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</Link></li>
-      <li className="nav-item"><Link to="/Kanbas" className="nav-link">Kanbas</Link></li>
-      <li className="nav-item"><Link id="wd-github" to="https://github.com/Alpha-Stark/kanbas-react-web-app" className="nav-link">GitHub</Link></li>
+      <li className="nav-item">
+        <a id="wd-a" href="#/Labs" className="nav-link">
+          Labs
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a1" href="#/Labs/Lab1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
+          Lab 1
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a2" href="#/Labs/Lab2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
+          Lab 2
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-a3" href="#/Labs/Lab3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
+          Lab 3
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-k" href="#/Kanbas" className="nav-link">
+          Kanbas
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+          My GitHub
+        </a>
+      </li>
     </ul>
-  )
+  );
 }
-
-export default TOC
