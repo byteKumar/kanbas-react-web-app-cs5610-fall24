e2799d06fc4c1a645c288ef6e8fbec29eb44c7fe
Chaman Kumar <chamansuper1@gmail.com>
2024-10-17 22:40:52 -0400

    Assignment 3 labs changes

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,20 +1,21 @@
 import { Navigate, Route, Routes } from "react-router";
 import Lab1 from "./Lab1";
-import Lab2 from "./Lab2";
 import TOC from "./TOC";
+import Lab3 from "./Lab3";
+import Lab2 from "./Lab2";
 
 export default function Labs() {
-    return (
-        <div>
-            <h1>Chaman Kumar</h1>
-            <h2>Labs</h2>
-            <TOC />
-            <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
-                <Route path="Lab1" element={<Lab1 />} />
-                <Route path="Lab2" element={<Lab2 />} />
-                <Route path="Lab3" element={<h2>Lab 3</h2>} />
-            </Routes>
-        </div>
-    );
-}
+  return (
+    <div id="wd-labs">
+      <h1>Chaman Kumar</h1>
+      <h1>Labs</h1>
+      <TOC />
+      <Routes>
+        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="Lab1" element={<Lab1 />} />
+        <Route path="Lab2" element={<Lab2 />} />
+        <Route path="Lab3" element={<Lab3 />} />
+      </Routes>
+
+    </div>
+);}
