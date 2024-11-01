0a4db67e5a51546d2fa9cce15ce053cc87efe094
Chaman Kumar <chamansuper1@gmail.com>
2024-10-31 21:06:01 -0400

    some changes

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -10,19 +10,25 @@ export default function TOC() {
       </li>
       <li className="nav-item">
         <a id="wd-a1" href="#/Labs/Lab1"
-          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
+          className={`nav-link 
+            ${pathname.includes("Lab1") ? "active" : ""}`
+            }>
           Lab 1
         </a>
       </li>
       <li className="nav-item">
         <a id="wd-a2" href="#/Labs/Lab2"
-          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
+          className={`nav-link 
+          ${pathname.includes("Lab2") ? "active" : ""}`
+          }>
           Lab 2
         </a>
       </li>
       <li className="nav-item">
         <a id="wd-a3" href="#/Labs/Lab3"
-          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
+          className={`nav-link 
+          ${pathname.includes("Lab3") ? "active" : ""}`
+          }>
           Lab 3
         </a>
       </li>
@@ -32,7 +38,9 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/Bhavyalakhani/kanbas-react-web-app" className="nav-link">
+        <a id="wd-k" 
+        href="https://github.com/byteKumar/kanbas-react-web-app-cs5610-fall24" 
+        className="nav-link">
           My GitHub
         </a>
       </li>
