cfc7a058fa51bb29f7abada2b88e27f8c3a0b96b
Chaman Kumar <chamansuper1@gmail.com>
2024-11-01 00:10:22 -0400

    Assignment 4 changes

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -9,36 +9,53 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a1" href="#/Labs/Lab1"
-          className={`nav-link 
-            ${pathname.includes("Lab1") ? "active" : ""}`
-            }>
+        <a
+          id="wd-a1"
+          href="#/Labs/Lab1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
+        >
           Lab 1
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a2" href="#/Labs/Lab2"
-          className={`nav-link 
-          ${pathname.includes("Lab2") ? "active" : ""}`
-          }>
+        <a
+          id="wd-a2"
+          href="#/Labs/Lab2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
+        >
           Lab 2
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab3"
-          className={`nav-link 
-          ${pathname.includes("Lab3") ? "active" : ""}`
-          }>
+        <a
+          id="wd-a3"
+          href="#/Labs/Lab3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
+        >
           Lab 3
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="#/Kanbas" className="nav-link"> Kanbas </a>
+        <a
+          id="wd-a4"
+          href="#/Labs/Lab4"
+          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}
+        >
+          Lab 4
+        </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" 
-        href="https://github.com/byteKumar/kanbas-react-web-app-cs5610-fall24" 
-        className="nav-link">
+        <a id="wd-k" href="#/Kanbas" className="nav-link">
+          Kanbas
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          id="wd-github"
+          href="https://github.com/byteKumar/kanbas-react-web-app-cs5610-fall24"
+          className="nav-link"
+          target="_blank"
+        >
           My GitHub
         </a>
       </li>
